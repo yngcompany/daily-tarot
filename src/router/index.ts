471df50adf8df8679d3da7797bacfb14a086b9ab
Jay Yu <yngcompany@gmail.com>
2025-12-13 20:09:45 +0900

    build test (오류 수정)

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import MainView from '@/views/MainView.vue'
 import ShuffleView from '@/views/ShuffleView.vue'
 import ResultView from '@/views/ResultView.vue'
@@ -14,7 +14,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes
 })
 
