17bb8892e9ffe95b9b35fc2d3bfeedc6d7cfdb2a
Jay Yu <yngcompany@gmail.com>
2025-12-13 21:22:14 +0900

    test

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,11 @@
-<script setup lang="ts">
-import { useAppStore } from '@/store/useAppStore'
-
-const appStore = useAppStore()
-</script>
-
 <template>
   <div :class="['app-root', appStore.theme]">
     <!-- 모든 화면은 여기로 들어온다 -->
-    <RouterView />
+    <router-view />
   </div>
 </template>
 
-<style lang="scss">
-</style>
+<script setup lang="ts">
+import { useAppStore } from '@/store/useAppStore'
+const appStore = useAppStore()
+</script>
