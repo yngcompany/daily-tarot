966de16db11f082de42a736f79cdeed99b90dab6
Jay Yu <yngcompany@gmail.com>
2025-12-13 20:05:53 +0900

    build test

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,12 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
-export default defineConfig(({ mode }) => {
-  return {
-    base: mode === 'production' ? '/repo-name/' : '/',
-    plugins: [vue()],
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, 'src'),
-      },
+export default defineConfig({
+  base: '/daily-tarot/',  // ← 반드시 저장소 이름과 맞춰야 함
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
     },
-  }
+  },
 })
